fe22d4225cdada956cf78f65c1f3fecbe1ddf4c9
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2020-09-06 23:25:40 +0900

    Initial Commit

---
+++ AlertDisplayer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlertDisplayer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string[] arg = Environment.GetCommandLineArgs();
+            if (arg.Length == 4)
+            {
+                MessageBoxIcon messageBoxIcon;
+                if (Enum.TryParse<MessageBoxIcon>(arg[3], out messageBoxIcon))
+                {
+                    MessageBox.Show(arg[1], arg[2], MessageBoxButtons.OK, messageBoxIcon,MessageBoxDefaultButton.Button1,MessageBoxOptions.DefaultDesktopOnly);
+                }
+            }
+        }
+    }
+}
